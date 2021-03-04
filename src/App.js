397df6dcaee3ad240e87f2bd9daaa508b734bd8c
Xinhan <huangxinhan99@gmail.com>
2021-03-04 11:07:14 -0500

    New York Congressional District geojson added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import ReactMapboxGL, { Source, Layer } from "@urbica/react-map-gl";
+import newyorkDistricts from "./geojson/New_York_Congressional_Districts.geojson"
 
 class App extends React.Component {
   state = {
@@ -20,7 +21,22 @@ class App extends React.Component {
           mapStyle="mapbox://styles/mapbox/light-v9"
           accessToken={'pk.eyJ1Ijoid29ybGRjYWxsaW5nIiwiYSI6ImNrbHV6ZW1mazA0YmMybnFvb3VxcXA0ejQifQ.t_Hw0W0XVxTZcbS4NsrFlw'}
           onViewportChange={viewport => this.setState({ viewport })}
-        ></ReactMapboxGL>
+        >
+        <Source
+          id="nydistricts"
+          type="geojson"
+          data={newyorkDistricts}
+        />
+        <Layer
+          id="NewYorkCongressionalDistricts"
+          type="fill"
+          source="nydistricts"
+          paint={{
+            "fill-color": "#228b22",
+            "fill-opacity": 0.4
+          }}
+        />
+        </ReactMapboxGL>
       </div>
     )
   }
