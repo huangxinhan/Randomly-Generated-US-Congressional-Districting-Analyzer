ff86221f94f0d4add1f7cc4e375a0c3d8e2cb6ef
Xinhan <huangxinhan99@gmail.com>
2021-03-05 18:58:15 -0500

    new york precinct using mapbox-gl instead of react

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,46 +1,67 @@
 import React, { useState, Component} from 'react'
 import ReactDOM from 'react-dom'
 import ReactMapboxGL, { Source, Layer } from "@urbica/react-map-gl";
-import marylandPrecincts from "../geojson/md_2016_w_ushouse.geojson"
+import marylandPrecincts from "../geojson/md_2016_w_ushouse.json"
+import nyprecincts from "../geojson/ny_final.json"
+import mapboxgl from "mapbox-gl"
+import L, { layerGroup } from 'leaflet'
+import "leaflet/dist/leaflet.css"
 
-
-
+let config = {};
+config.params = {
+  center: [39.8283,-98.5795],
+  zoomControl: false,
+  zoom: 4.25,
+  maxZoom: 19,
+  minZoom: 1,
+  scrollwheel: false,
+  legends: true,
+  infoControl: false,
+  attributionControl: true
+};
+config.tileLayer = {
+  url: 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.pn',
+};
 
 class Maps extends Component{
-    state = {
-        viewport: {
-          latitude: 39.8283,
-          longitude: -98.5795,
-          zoom: 4.25
+  constructor(props) {
+    super(props)
+    this.state = {
+      map: null,
+      tileLayer: null,
+      geojsonLayer: null,
+      geojson: null,
+    };
+    this._mapNode = null;
+  }
+
+  componentDidMount(){
+    var map = L.map('map').setView([37.8, -96], 5)
+    map.zoomControl.setPosition('topleft')
+    L.tileLayer('https://api.mapbox.com/styles/v1/worldcalling/cklvc0h5648r517o49ebf9d6q/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoid29ybGRjYWxsaW5nIiwiYSI6ImNrbHZjbjV4cjJvcXYycHBtMmJjaGZ0aHcifQ.68N60kfWy9s3PeNMuqnuQA').addTo(map)
+
+    L.geoJson(nyprecincts, {
+      style: function(feature) {
+        if (feature.properties){
+          return {color: 'red', opacity:0.7}
         }
-      };
+      },
+      onEachFeature: onEachStateFeature
+    }).addTo(map);
+
+    function onEachStateFeature(feature, layer) {
+      layer.on('click', function(e) {
+        if (feature.properties){
+        }
+      })
+    }
+
     
-      render(){
+  }
+
+    render(){
         return(
-          <div>
-            <ReactMapboxGL
-              {...this.state.viewport}
-              style={{width: "100vw", height: "100vh"}}
-              mapStyle="mapbox://styles/mapbox/light-v9"
-              accessToken={'pk.eyJ1Ijoid29ybGRjYWxsaW5nIiwiYSI6ImNrbHZjbjV4cjJvcXYycHBtMmJjaGZ0aHcifQ.68N60kfWy9s3PeNMuqnuQA'}
-              onViewportChange={viewport => this.setState({ viewport })}
-            >
-            <Source
-              id="mdprecincts"
-              type="geojson"
-              data={marylandPrecincts}
-            />
-            <Layer
-              id="MarylandPrecincts"
-              type="fill"
-              source="mdprecincts"
-              paint={{
-                "fill-color": "#228b22",
-                "fill-opacity": 0.4
-              }}
-            />
-            </ReactMapboxGL>
-          </div>
+          <div id="map" style={{ width: '100vw', height: '100vh'}}></div>
         )
       }
     }
