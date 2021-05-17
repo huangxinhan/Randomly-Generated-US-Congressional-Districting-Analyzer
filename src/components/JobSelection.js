3e3fcc7c337789b78e4b8a1c7ce8637692006bd3
Zhenghao Fang <f97zhh@gmail.com>
2021-05-17 00:34:08 -0400

    jobsummary

--- src/components/JobSelection.js
+++ src/components/JobSelection.js
@@ -9,9 +9,11 @@ class JobSelection extends Component {
   }
 
   render() {
-    let job1=this.props.mgggPrams;
-    //console.log(mgggs);
-    //let job1=mgggs[0];
+    let mgggs=this.props.mgggPrams;
+    console.log(mgggs);
+    let job1=mgggs[0];
+    let job2=mgggs[1];
+    let job3=mgggs[2];
     let states={jobSummaryID: "job1", stateName: "PA", numOfDistrictings: "103201", numOfDistricts: "27", coolingRounds: "3",totalRounds:"150"};
     
     return (
@@ -33,10 +35,10 @@ class JobSelection extends Component {
 
         <div class="card bg-light mb-3" style={{ width: "550px", fontSize: "15px", left: "13px" }}>
           <div class="card-body">
-          <h5 class="card-title">{states.stateName} Job2</h5>
-            <p class="card-text">•{states.numOfDistrictings} districtings  •{states.numOfDistricts} districts   </p>
-            <p class="card-text">• Cooling Rounds: {states.coolingRounds} </p>
-            <p class="card-text">•TotalRounds: {states.totalRounds}   </p>
+          <h5 class="card-title">{job2.stateName} Job2</h5>
+            <p class="card-text">•{job2.numOfDistrictings} districtings  •{job2.numOfDistricts} districts   </p>
+            <p class="card-text">• Cooling Rounds: {job2.coolingRounds} </p>
+            <p class="card-text">•TotalRounds: {job2.totalRounds}   </p>
             <input class="form-check-input" type="radio" value="2" id="flexCheckDefault 2" checked={this.props.jobChecked===2?true :false} onClick={this.props.selectJobs(2)}/>
             <label class="form-check-label" for="flexCheckDefault">
               Select Job
@@ -46,10 +48,10 @@ class JobSelection extends Component {
 
         <div class="card bg-light mb-3" style={{ width: "550px", fontSize: "15px", left: "13px" }}>
           <div class="card-body">
-          <h5 class="card-title">{states.stateName} Job3</h5>
-            <p class="card-text">•{states.numOfDistrictings} districtings  •{states.numOfDistricts} districts   </p>
-            <p class="card-text">• Cooling Rounds: {states.coolingRounds} </p>
-            <p class="card-text">•TotalRounds: {states.totalRounds}   </p>
+          <h5 class="card-title">{job3.stateName} Job3</h5>
+            <p class="card-text">•{job3.numOfDistrictings} districtings  •{job3.numOfDistricts} districts   </p>
+            <p class="card-text">• Cooling Rounds: {job3.coolingRounds} </p>
+            <p class="card-text">•TotalRounds: {job3.totalRounds}   </p>
             <input class="form-check-input" type="radio" value="3" id="flexCheckDefault 3" checked={this.props.jobChecked===3?true :false} onClick={this.props.selectJobs(3)}/>
             <label class="form-check-label" for="flexCheckDefault">
               Select Job
